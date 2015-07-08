e798a26980abdb03213d126542de87e932f9b6cf
Etienne Neveu <etienne@instaply.com>
2015-07-08 15:39:58 +0200

    Configure QueryDSL.

--- src/main/java/com/paradigmkick/sample/querydsl/QuerydslSampleApplication.java
+++ src/main/java/com/paradigmkick/sample/querydsl/QuerydslSampleApplication.java
@@ -1,12 +1,25 @@
 package com.paradigmkick.sample.querydsl;
 
+import com.mysema.query.jpa.JPQLQueryFactory;
+import com.mysema.query.jpa.impl.JPAProvider;
+import com.mysema.query.jpa.impl.JPAQueryFactory;
+
+import javax.inject.Provider;
+import javax.persistence.EntityManager;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class QuerydslSampleApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(QuerydslSampleApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(QuerydslSampleApplication.class, args);
+  }
+
+  @Bean
+  public JPQLQueryFactory queryFactoryForQueryDsl(Provider<EntityManager> entityManager) {
+    return new JPAQueryFactory(JPAProvider.getTemplates(entityManager.get()), entityManager);
+  }
 }
