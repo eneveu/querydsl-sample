736942de81e18d4a1e8063c8b4c27009f6952ed8
Etienne Neveu <etienne@instaply.com>
2015-07-08 10:57:46 +0200

    Generate sample project using Spring Initializr

---
+++ src/main/java/com/paradigmkick/sample/querydsl/QuerydslSampleApplication.java
@@ -0,0 +1,12 @@
+package com.paradigmkick.sample.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslSampleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/paradigmkick/sample/querydsl/QuerydslSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.paradigmkick.sample.querydsl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = QuerydslSampleApplication.class)
+public class QuerydslSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
